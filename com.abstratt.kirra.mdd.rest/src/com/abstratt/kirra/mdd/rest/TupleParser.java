4b8fba00f615d24f58b627a2f62105c3ea25f0de
Rafael Chaves <rafael@abstratt.com>
2014-07-09 16:37:58 -0300

    parsing JSON as tuples

--- com.abstratt.kirra.mdd.rest/src/com/abstratt/kirra/mdd/rest/TupleParser.java
+++ com.abstratt.kirra.mdd.rest/src/com/abstratt/kirra/mdd/rest/TupleParser.java
@@ -8,7 +8,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
 import org.codehaus.jackson.JsonFactory;
@@ -20,8 +23,11 @@ import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.ObjectNode;
 
+import com.abstratt.kirra.DataScope;
 import com.abstratt.kirra.Entity;
+import com.abstratt.kirra.Helper;
 import com.abstratt.kirra.Instance;
 import com.abstratt.kirra.KirraException;
 import com.abstratt.kirra.Property;
@@ -33,8 +39,14 @@ import com.abstratt.kirra.TupleType;
 import com.abstratt.kirra.TypeRef;
 
 public class TupleParser {
+    
+    private SchemaManagement schemaManagement;
+    
+    public TupleParser(SchemaManagement schemaManagement) {
+        this.schemaManagement = schemaManagement;
+    }
 
-    public static Object convertSlotValue(String slotName, JsonNode slotValueNode) {
+    public Object convertSlotValue(TypeRef expectedType, JsonNode slotValueNode) {
         Object fieldValue = null;
         if (slotValueNode == null) {
             fieldValue = null;
@@ -49,12 +61,30 @@ public class TupleParser {
         } else if (slotValueNode.isNull()) {
             fieldValue = null;
         } else if (slotValueNode.isArray()) {
-            fieldValue = null;
+            ArrayList<Object> array = new ArrayList<Object>();
+            ArrayNode asArray = (ArrayNode) slotValueNode;
+            for (JsonNode jsonNode : asArray) {
+                array.add(convertSlotValue(expectedType, jsonNode));
+            }
+            fieldValue = array;
+        } else if (slotValueNode.isObject()) {
+            TupleType expectedTupleType = schemaManagement.getTupleType(expectedType);
+            Map<String, Object> map = new LinkedHashMap<String, Object>();
+            ObjectNode asObject = (ObjectNode) slotValueNode;
+            for (Iterator<Map.Entry<String, JsonNode>> entries = asObject.getFields(); entries.hasNext(); ) {
+                Entry<String, JsonNode> entry = entries.next();
+                Property entryProperty = expectedTupleType.getProperty(entry.getKey());
+                if (entryProperty != null)
+                    map.put(entry.getKey(), convertSlotValue(entryProperty.getTypeRef(), entry.getValue()));
+            }
+            Tuple tuple = new Tuple(expectedTupleType.getTypeRef());
+            tuple.setValues(map);
+            fieldValue = tuple;
         }
         return fieldValue;
     }
 
-    public static Tuple getTupleFromJsonRepresentation(JsonNode tupleRepresentation, TupleType tupleType) {
+    public Tuple getTupleFromJsonRepresentation(JsonNode tupleRepresentation, TupleType tupleType) {
         Tuple newTuple = new Tuple(tupleType.getTypeRef());
         if (tupleRepresentation == null || tupleRepresentation.isNull())
             return null;
@@ -64,12 +94,12 @@ public class TupleParser {
             JsonNode fieldValueNode = tupleRepresentation.get(fieldName);
             Property property = tupleType.getProperty(fieldName);
             if (property != null)
-                TupleParser.setProperty(newTuple, fieldName, fieldValueNode);
+                setProperty(newTuple, fieldName, fieldValueNode);
         }
         return newTuple;
     }
 
-    public static List<?> getValuesFromJsonRepresentation(SchemaManagement repository, JsonNode representation, TypeRef type,
+    public List<?> getValuesFromJsonRepresentation(JsonNode representation, TypeRef type,
             boolean multiple) {
         if (representation == null)
             return null;
@@ -77,9 +107,8 @@ public class TupleParser {
         switch (type.getKind()) {
         case Tuple:
         case Entity:
-            TupleType tupleType = repository.getTupleType(type);
-            return multiple ? TupleParser.getTuplesFromJsonRepresentation(asArray, tupleType) : Arrays.asList(TupleParser
-                    .getTupleFromJsonRepresentation(representation, tupleType));
+            TupleType tupleType = schemaManagement.getTupleType(type);
+            return multiple ? getTuplesFromJsonRepresentation(asArray, tupleType) : Arrays.asList(getTupleFromJsonRepresentation(representation, tupleType));
         case Primitive:
         case Enumeration:
             return multiple ? TupleParser.getPrimitiveValuesFromJsonRepresentation(asArray) : Arrays.asList(TupleParser
@@ -105,13 +134,13 @@ public class TupleParser {
         }
     }
 
-    public static Instance resolveLink(JsonNode fieldValueNode, TypeRef type) {
+    public Instance resolveLink(JsonNode fieldValueNode, TypeRef type) {
         if (fieldValueNode == null || fieldValueNode.isNull())
             return null;
-        return TupleParser.resolveLink(fieldValueNode.getTextValue(), type);
+        return resolveLink(fieldValueNode.getTextValue(), type);
     }
 
-    public static Instance resolveLink(String uriString, TypeRef type) {
+    public Instance resolveLink(String uriString, TypeRef type) {
         if (uriString == null || uriString.trim().isEmpty())
             return null;
         String objectId = null;
@@ -124,7 +153,7 @@ public class TupleParser {
         return toLink;
     }
 
-    public static void updateInstanceFromJsonRepresentation(JsonNode toUpdate, Entity entity, Instance existingInstance) {
+    public void updateInstanceFromJsonRepresentation(JsonNode toUpdate, Entity entity, Instance existingInstance) {
         JsonNode values = toUpdate.get("values");
         if (values == null || values.isNull())
             return;
@@ -135,34 +164,37 @@ public class TupleParser {
             Property property = entity.getProperty(fieldName);
             if (property != null) {
                 if (property.isEditable())
-                    TupleParser.setProperty(existingInstance, fieldName, fieldValueNode);
+                    setProperty(existingInstance, fieldName, fieldValueNode);
             } else {
                 Relationship relationship = entity.getRelationship(fieldName);
                 if (relationship != null)
                     if (relationship.getStyle() != Style.PARENT && relationship.isEditable())
-                        TupleParser.updateRelationship(entity, existingInstance, relationship, fieldValueNode);
+                        updateRelationship(entity, existingInstance, relationship, fieldValueNode);
             }
         }
     }
 
-    public static void updateRelationship(Entity entity, Instance existingInstance, Relationship relationship, JsonNode fieldValueNode) {
+    public void updateRelationship(Entity entity, Instance existingInstance, Relationship relationship, JsonNode fieldValueNode) {
         if (fieldValueNode == null || fieldValueNode.isNull())
             return;
         if (fieldValueNode.isTextual())
-            existingInstance.setSingleRelated(relationship.getName(), TupleParser.resolveLink(fieldValueNode, relationship.getTypeRef()));
+            existingInstance.setSingleRelated(relationship.getName(), resolveLink(fieldValueNode, relationship.getTypeRef()));
         else if (fieldValueNode.isArray()) {
             List<Instance> allRelated = new ArrayList<Instance>();
             for (Iterator<JsonNode> elements = ((ArrayNode) fieldValueNode).getElements(); elements.hasNext();)
-                allRelated.add(TupleParser.resolveLink(elements.next(), relationship.getTypeRef()));
+                allRelated.add(resolveLink(elements.next(), relationship.getTypeRef()));
             existingInstance.setRelated(relationship.getName(), allRelated);
         } else if (fieldValueNode.isObject()) {
             existingInstance.setSingleRelated(relationship.getName(),
-                    TupleParser.resolveLink(fieldValueNode.get("uri"), relationship.getTypeRef()));
+                    resolveLink(fieldValueNode.get("uri"), relationship.getTypeRef()));
         }
     }
 
-    protected static void setProperty(Tuple record, String fieldName, JsonNode fieldValueNode) {
-        record.setValue(fieldName, TupleParser.convertSlotValue(fieldName, fieldValueNode));
+    protected void setProperty(Tuple record, String fieldName, JsonNode fieldValueNode) {
+        DataScope dataScope = Helper.resolveDataScope(schemaManagement, record.getTypeRef());
+        Property property = dataScope.getProperty(fieldName);
+        if (property != null)
+            record.setValue(fieldName, convertSlotValue(property.getTypeRef(), fieldValueNode));
     }
 
     private static Object getPrimitiveValueFromJsonRepresentation(JsonNode jsonNode) {
@@ -179,14 +211,14 @@ public class TupleParser {
     private static List<?> getPrimitiveValuesFromJsonRepresentation(ArrayNode asArray) {
         List values = new ArrayList();
         for (JsonNode jsonNode : asArray)
-            values.add(TupleParser.getPrimitiveValueFromJsonRepresentation(jsonNode));
+            values.add(getPrimitiveValueFromJsonRepresentation(jsonNode));
         return values;
     }
 
-    private static List<?> getTuplesFromJsonRepresentation(ArrayNode asArray, TupleType tupleType) {
+    private List<?> getTuplesFromJsonRepresentation(ArrayNode asArray, TupleType tupleType) {
         List values = new ArrayList();
         for (JsonNode jsonNode : asArray)
-            values.add(TupleParser.getTupleFromJsonRepresentation(jsonNode, tupleType));
+            values.add(getTupleFromJsonRepresentation(jsonNode, tupleType));
         return values;
     }
 
