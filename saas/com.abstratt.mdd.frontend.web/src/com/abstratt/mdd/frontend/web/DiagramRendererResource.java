5dd940be2dc2c2fde5e40dd96eeaa162893fae60
Rafael Chaves <rafael@abstratt.com>
2015-06-07 09:58:17 -0300

    Some elements don't get rendered after a while #53
    Loading model contents as a resource task

--- saas/com.abstratt.mdd.frontend.web/src/com/abstratt/mdd/frontend/web/DiagramRendererResource.java
+++ saas/com.abstratt.mdd.frontend.web/src/com/abstratt/mdd/frontend/web/DiagramRendererResource.java
@@ -9,9 +9,12 @@ import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.SortedMap;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
@@ -19,9 +22,11 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.uml2.uml.Package;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
 import org.restlet.engine.adapter.HttpRequest;
@@ -34,10 +39,14 @@ import org.restlet.resource.Get;
 
 import com.abstratt.graphviz.GraphViz;
 import com.abstratt.mdd.core.IRepository;
+import com.abstratt.mdd.core.RepositoryService;
+import com.abstratt.mdd.core.util.MDDUtil;
 import com.abstratt.mdd.modelrenderer.uml2dot.UML2DOT;
 import com.abstratt.modelrenderer.MapBackedSettingsSource;
 import com.abstratt.modelrenderer.RenderingSettings;
 import com.abstratt.pluginutils.LogUtils;
+import com.abstratt.resman.Resource;
+import com.abstratt.resman.Task;
 
 public class DiagramRendererResource extends AbstractWorkspaceResource {
     private static final String DIAGRAM_SETTING_PREFIX = "mdd.diagram.";
@@ -49,35 +58,40 @@ public class DiagramRendererResource extends AbstractWorkspaceResource {
 			setStatus(Status.CLIENT_ERROR_NOT_FOUND);
 			return new EmptyRepresentation();
 		}
-		
 		checkSecret();
-		String fileName = (String) getRequestAttributes().get("file");
-		URI fileURI = URI.create("/" + fileName);
-		File modelFile = new File(workspaceDir, fileURI.getPath());
-		if (!modelFile.getParentFile().equals(workspaceDir) || !modelFile.isFile() || !modelFile.getName().endsWith(".uml")) {
-			setStatus(Status.CLIENT_ERROR_NOT_FOUND);
-		    return new EmptyRepresentation();
-		}
-		URI modelURI = modelFile.toURI();
-    	byte[] dotContents;
-		try {
-	        Map<String, String> diagramSettings = new HashMap<String, String>();
-	        diagramSettings.putAll(getQuery().getValuesMap());
-	        // project settings win
-            loadDiagramSettings(diagramSettings, new File(modelFile.getParentFile(), IRepository.MDD_PROPERTIES));
-            RenderingSettings settings = new RenderingSettings(new MapBackedSettingsSource(diagramSettings));
-			dotContents = UML2DOT.generateDOTFromModel(modelURI, settings);
-			String expectedContentType = ((HttpRequest) getRequest())
-					.getHttpCall().getRequestHeaders().getValues(
-							HeaderConstants.HEADER_ACCEPT);
-			if (!MediaType.TEXT_PLAIN.getName().equals(expectedContentType))
-				return convertDotToImageUsingGraphviz(dotContents);
-			return new StringRepresentation(new String(dotContents));
-		} catch (CoreException e) {
-            LogUtils.logWarning(getClass().getPackage().getName(), "Core error", e);
-            setStatus(Status.SERVER_ERROR_INTERNAL);
-            return ResourceUtils.buildErrorResponse(e);
-		}
+		String packageFile = (String) getRequestAttributes().get("file");
+		String packageName = FilenameUtils.removeExtension(packageFile);
+		
+		return RepositoryService.DEFAULT.runTask(MDDUtil.fromJavaToEMF(workspaceDir.toURI()), new Task<Representation>() {
+            @Override
+            public Representation run(Resource<?> resource) {
+                IRepository repository = RepositoryService.DEFAULT.getCurrentRepository();
+                File repositoryLocation = new File(repository.getBaseURI().toFileString());
+                Package packageToRender = repository.findPackage(packageName, null);
+                ResourceUtils.ensure(packageToRender != null, "Package not found: " + packageName, Status.CLIENT_ERROR_NOT_FOUND);
+                Map<String, String> diagramSettings = new HashMap<String, String>();
+                diagramSettings.putAll(getQuery().getValuesMap());
+                // project settings win
+                loadDiagramSettings(diagramSettings, new File(repositoryLocation, IRepository.MDD_PROPERTIES));
+                RenderingSettings settings = new RenderingSettings(new MapBackedSettingsSource(diagramSettings));
+                URI packageUri = URI.create(packageToRender.getURI());
+                byte[] dotContents;
+                try {
+                    dotContents = UML2DOT.generateDOTFromModel(packageUri, Arrays.asList(packageToRender), settings);
+                } catch (CoreException e) {
+                    ResourceUtils.fail(e, null);
+                    // never runs
+                    return null;
+                }
+                String expectedContentType = ((HttpRequest) getRequest())
+                        .getHttpCall().getRequestHeaders().getValues(
+                                HeaderConstants.HEADER_ACCEPT);
+                boolean textOutputExpected = MediaType.TEXT_PLAIN.getName().equals(expectedContentType);
+                if (textOutputExpected)
+                    return new StringRepresentation(new String(dotContents));
+                return convertDotToImageUsingGraphviz(dotContents);
+            }
+		});
     }
 
     private void loadDiagramSettings(Map<String, String> diagramSettings, File propertiesFile) {
