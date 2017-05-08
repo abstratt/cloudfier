c52b5114fa81ce230616fedaaf85740bd9353d00
Rafael Chaves <rafael@abstratt.com>
2017-05-08 17:08:03 -0300

    minor

--- runtime/com.abstratt.mdd.runtime.core/src/com/abstratt/mdd/core/runtime/types/BuiltInMetaClass.java
+++ runtime/com.abstratt.mdd.runtime.core/src/com/abstratt/mdd/core/runtime/types/BuiltInMetaClass.java
@@ -7,8 +7,8 @@ import com.abstratt.mdd.core.runtime.MetaClass;
 import com.abstratt.mdd.core.runtime.RuntimeEvent;
 
 public enum BuiltInMetaClass implements MetaClass<BuiltInClass> {
-    System("mdd_types::System", SystemType.class), Assert("mdd_types::Assert", AssertType.class), Boolean("mdd_types::Boolean",
-            BooleanType.class), Integer("mdd_types::Integer", IntegerType.class), Decimal("mdd_types::Double", RealType.class), String(
+    System("mdd_types::System", SystemType.class), Assert("mdd_types::Assert", AssertType.class), Console("mdd_console::Console", ConsoleType.class), 
+    		Boolean("mdd_types::Boolean", BooleanType.class), Integer("mdd_types::Integer", IntegerType.class), Decimal("mdd_types::Double", RealType.class), String(
             "mdd_types::String", StringType.class), Memo("mdd_types::Memo", MemoType.class), Image("mdd_media::Image", PictureType.class), Duration("mdd_types::Duration", DurationType.class), Date("mdd_types::Date", DateType.class), Set(
             "mdd_collections::Set", SetType.class), Bag("mdd_collections::Bag", BagType.class), OrderedSet("mdd_collections::OrderedSet",
             OrderedSetType.class), Sequence("mdd_collections::Sequence", SequenceType.class), Grouping("mdd_collections::Grouping",
